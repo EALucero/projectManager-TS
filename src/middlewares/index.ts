8f99825b2c98051931e3d87013e561b99a76727c
EALucero <56940163+EALucero@users.noreply.github.com>
2024-02-19 04:08:26 -0300

    Project controller (#4)
    * c7
    
    * c7

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -4,19 +4,12 @@ import { errorResponse, TokenInterface } from "../helpers";
 import User from "../models/User";
 import createHttpError from "http-errors";
 
-export interface CustomRequest extends Request {
-    user : {
-        name : string;
-    }                
-}
-
-export const checkAuth = async (req: CustomRequest, res: Response, next: NextFunction) => {
+export const checkAuth = async (req: Request, res: Response, next: NextFunction) => {
     let token;
 
     if(req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
         try {
             token = req.headers.authorization.split(' ')[1];
-
             const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
        
             req.user = await User.findById((decoded as TokenInterface).id).select(
