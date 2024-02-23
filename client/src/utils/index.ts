f5c996eb242311b009e5befaf6ad49ba8b9e8d1a
EALucero <56940163+EALucero@users.noreply.github.com>
2024-02-23 18:16:25 -0300

    projectCrud (#10)
    * c8
    
    * projectCrud
    
    * fix

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,16 +1,41 @@
 import Swal from "sweetalert2";
 
 type Icon = 'warning' | 'error' | 'success' | 'info' | 'question';
-type ShowMessageParams = (title: string, text: string, icon?: Icon, redirect? : () => void,) => void
+type Position = 'top' | 'top-start' | 'top-end' | 'center' | 'center' | 'center-start' | 'center-end' | 'bottom' | 'bottom-start' | 'bottom-end';
+
+type ShowMessageParams = (
+    title: string,
+    text: string,
+    icon?: Icon,
+    redirect?: () => void
+) => void
 
 export const showMessageResponse: ShowMessageParams = (title, text, icon = 'info', redirect) => {
     Swal.fire({
-        icon,
         title,
-        text
+        text,
+        icon
     }).then((result) => {
         if (result.isConfirmed) {
-           redirect && redirect()
-          }
+            redirect && redirect()
+        }
     })
+}
+
+type ShowToastParams = (
+    title: string,
+    position?: Position,
+    icon?: Icon,
+    showConfirmButton?: boolean,
+    timer?: number
+) => void;
+
+export const showToastMessage: ShowToastParams = (title, position = 'top-end', icon = 'success', showConfirmButton = false, timer = 1500) => {
+    Swal.fire({
+        title,
+        position,
+        icon,
+        showConfirmButton,
+        timer
+    });
 }
